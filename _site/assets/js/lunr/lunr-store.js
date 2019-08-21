bb99110bbaa245c73d2573b7b3f043a0d4a9cf1a
YONGJIN SHIN <yongjin.shinn@gmail.com>
2019-08-21 09:40:26 +0900

    syntax error

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "test page for github.io",
+        "excerpt":"This is my first github blog’s post and my first blog ever.   The title of this post is test page for github.io.  ","categories": ["CAT"],
+        "tags": ["TAG"],
+        "url": "http://localhost:4000/cat/test-post/",
+        "teaser":null}]
