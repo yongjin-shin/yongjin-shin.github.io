bc70474d7d9a36c48ffa4e3466a5721e634acb68
YONGJIN SHIN <yongjin.shinn@gmail.com>
2019-08-21 11:16:39 +0900

    set up

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "test page for github.io",
-        "excerpt":"This is my first github blog’s post and my first blog ever.   The title of this post is test page for github.io.  ","categories": ["CAT"],
-        "tags": ["TAG"],
-        "url": "http://localhost:4000/cat/test-post/",
+        "title": "start-blogging",
+        "excerpt":"First Post ever!  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "http://localhost:4000/blog/test/",
         "teaser":null}]
