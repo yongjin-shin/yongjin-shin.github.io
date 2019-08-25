84e1ec4464ea30a45ee130d3c1e772369610062c
YONGJIN SHIN <yongjin.shinn@gmail.com>
2019-08-25 19:22:32 +0900

    font size

--- _pages/about.md
+++ _pages/about.md
@@ -3,8 +3,10 @@ title: ""
 permalink: /about/
 layout: single
 ---
-
 ### Blogging with minimal effort..!
+
 My background is in industrial engineering and computer science.
 My research interests are: 
 Multi-agent Systems, Reinforcement Learning, and Game Theory.
+
+email: yj.shin[at]kaist[dot]ac[dot]kr
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"First Post ever!  ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/test/",
+        "teaser":null},{
+        "title": "성공하는 대학원생의 3가지 자질: 끈기, 고집 그리고 설득력",
+        "excerpt":"매년 가을, 대학원 신입생들이 온다. 나는 신입생들을 ~ 하기 때문에 Since I’m actively looking for Ph.D, “박사 졸업은 얼마나 걸리나요?”라는 질문을 매년 받는다. 나는 “대학원은 자네가 원하는 만큼 오래 다닐 수 있네”라고 답한다. 졸업에 필요한 자격 요건을 갖추는데에는 속도제한 따위는 없기 대문이다. 그러나 이 질문은 옳은 질문이라 할 수 없다....","categories": ["번역"],
+        "tags": ["번역"],
+        "url": "http://localhost:4000/%EB%B2%88%EC%97%AD/Translation1/",
         "teaser":null}]
