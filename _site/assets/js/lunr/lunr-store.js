fb585336055fd8a0b78383e4722266b4c30039a6
YONGJIN SHIN <yongjin.shinn@gmail.com>
2019-08-30 22:59:08 +0900

    resolved error

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,14 @@ var store = [{
         "excerpt":"간만에 연구실 출근해서 컴퓨터에 우분투18.04+윈도우10 설치를 했습니다. 원래 맥북을 써왔는데 연구실에서 윈도우를 쓰려니 도무지 적응이 안되어서 벼르고 있던 우분투를 깔아버렸네요. 제대로 설치해보기는 처음인지라 한참을 헤매고 다녀서 오늘 하루 시간만 날려먹었네요. 설치에 도움이 되었던 자료들만 간략하게 정리를 해봅니다 (조만간 잘 못 만져서 갈아엎을 때 필요할 것 같으므로…) 우분투 설치: https://cupjoo.tistory.com/53 고정...","categories": ["Blog"],
         "tags": ["팁","우분투"],
         "url": "http://localhost:4000/blog/tip1/",
+        "teaser":null},{
+        "title": "[Summary] Multi-agent Systems-Chapter4",
+        "excerpt":"3장에서는 게임에 있어서 최적 솔루션이 무엇인가(혹은 Nash Euilibrium) 이외에 다른 대안이 없는지에 대해서다. 4장에서는 그 솔루션을 얻는데 있어 계산이 얼마나 복잡할 것인가에 대한 문제를 다루게 된다. 자연스럽게 가장 간단한 2명의 플레이어, 제로섬 normal-form게임으로부터 시작하여, general-sum게임과 n플레이어+general-sum게임으로까지 확장된다. 더불어 3장에서 살펴보았던 maxmin/minmax전략과 우위전략, correlated equilbria 전략까지 솔루션을 얻는데 얼마나 복잡한지에 대한...","categories": ["Book"],
+        "tags": ["MAS","Summary"],
+        "url": "http://localhost:4000/book/mas-chap4/",
+        "teaser":null},{
+        "title": "[Summary] An Intro Lin.Prog & Game Theory-Chapter8",
+        "excerpt":"Multi-agent Systems의 극악무도한 내용 때문에 다른 강의들 뒤적이다가 뭔가 LP를 다시 봐야할 것 같아서, 예전에 대학원 시험 때문에 공부했던 “An Introduction Linear Programming and Game Theory”를 다시 펼쳐봤습니다..신세계를 맛보았네요. 아주 아주 간략한 포인트만 정리해두도록 하겠습니다. (참고로 이 책에서는 오직 Two Person Zero Sum game만을 다루고 있어서, 결국 그 책으로 돌아가야만...","categories": ["MAS","Game Theory"],
+        "tags": ["Book","Summary"],
+        "url": "http://localhost:4000/mas/game%20theory/lp-gt1/",
         "teaser":null}]
